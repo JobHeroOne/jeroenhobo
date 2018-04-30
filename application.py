307113b570d8cd8031e03cff1705aa2a598b3a56
Unknown <hobo.jeroen@gmail.com>
2018-04-30 19:04:52 +0200

    Adding Heartrate Page

--- application.py
+++ application.py
@@ -8,5 +8,11 @@ app = Flask(__name__)
 def index():
 	return render_template("index.html")
 
+
+@app.route("/heartrate")
+def heartrate():
+	return render_template("heartrate.html")
+
+
 if __name__ == "__main__":
 	app.run()
